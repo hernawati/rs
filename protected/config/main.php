b237ce3cb29af78179488c56b6ffdc057b76bec3
unknown <herna@herna-PC.(none)>
2013-11-20 23:54:10 +0700

    versi ter-update

--- protected/config/main.php
+++ protected/config/main.php
@@ -56,7 +56,7 @@ return array(
 		// uncomment the following to use a MySQL database
 		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=coba',
+			'connectionString' => 'mysql:host=localhost;dbname=rs',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
